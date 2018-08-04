c524839a94b2d50a7a3bad89ef2482b63f7335e2
municorn <municorn@musicaloft.com>
2018-08-03 20:55:02 -0600

    added help menu

--- main.go
+++ main.go
@@ -7,8 +7,22 @@ import (
 func main() {
 	var tasks []Task
 	fmt.Println("Welcome to Mivy!")
+	addTask(&tasks)
+	tasks[0].Display()
+}
+
+func addTask(dest *[]Task) {
 	var foo Task
 	foo.Prompt()
-	tasks = append(tasks, foo)
-	tasks[0].Display();
+	*dest = append(*dest, foo)
+}
+
+func displayHelp() {
+	fmt.Println("Here's what you can do:");
+	fmt.Println("\ta\tadd a task");
+	fmt.Println("\te\tedit a task");
+	fmt.Println("\td\tdelete a task");
+	fmt.Println("\tv\tview a todo list");
+	fmt.Println("\ts\tsettings");
+	fmt.Println("\th\thelp");
 }
