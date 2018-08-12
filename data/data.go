9c2ed177e73aacaaaa6732a732000439278aff67
municorn <municorn@musicaloft.com>
2018-08-11 21:48:08 -0600

    added data.go

---
+++ data/data.go
@@ -0,0 +1,14 @@
+package data
+
+import "mivy"
+
+// ReadData returns a slice of Tasks
+func ReadData() []Task {
+	var tasks []Task
+	return tasks
+}
+
+// WriteData writes the user's data
+func WriteData() {
+
+}
