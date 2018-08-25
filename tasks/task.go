5b1a3c9382a950cc6d81de515a01382d83d9c08d
municorn <municorn@musicaloft.com>
2018-08-25 15:16:03 -0600

    fixed some bugs with adding and viewing tasks

--- tasks/task.go
+++ tasks/task.go
@@ -1,11 +1,12 @@
 package tasks
 
 import (
-	"fmt"
-	"strconv"
-	"time"
 	"bufio"
+	"fmt"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 // Task is a task. this comment is just here to make gofmt happy
@@ -21,6 +22,7 @@ type Task struct {
 // Prompt prompts the user for the information of Task t
 func (t *Task) Prompt() {
 
+	// PROMPT FOR NAME
 	in := bufio.NewReader(os.Stdin)
 	fmt.Print("\nWhat's the name of this task? ")
 	var err error
@@ -28,15 +30,12 @@ func (t *Task) Prompt() {
 	if err != nil {
 		panic(err) // we shouldn't do this but oh well lol
 	}
+	// trim whitespace and stuff
+	t.Name = strings.TrimSpace(t.Name)
 
-	fmt.Print("How many days minimum will it take to complete? (default is 3) ")
+	//////////////////////////
 
-	// n is the number of elements read in. if nothing is read (n == 0),
-	// default t.UserSpan to 3
-	if n, _ := fmt.Scanln(&t.UserSpan); n == 0 {
-		t.UserSpan = 3
-	}
-
+	// PROMPT FOR DUE DAY
 	var dueInput string
 	fmt.Print("When is this task due? (mm/dd/yyyy) ")
 	fmt.Scanln(&dueInput)
@@ -45,8 +44,19 @@ func (t *Task) Prompt() {
 	if err != nil {
 		_ = fmt.Errorf(err.Error())
 	}
-	t.UserDueDay = int(dueTime.Unix()/(3600*24)) + 1 //for some reason we just have to add one here
+	t.UserDueDay = int(dueTime.Unix()/(3600*24)) + 1 // for some reason we just have to add one here
 	fmt.Println("\t===>added task for day", t.UserDueDay)
+	///////////////////////////
+
+	// PROMPT FOR SPAN
+	fmt.Print("How many days minimum will it take to complete? (default is 3) ")
+
+	// n is the number of elements read in. if nothing is read (n == 0),
+	// default t.UserSpan to 3
+	if n, _ := fmt.Scanln(&t.UserSpan); n == 0 {
+		t.UserSpan = 3
+	}
+	///////////////////////////
 
 	// default modifieds to the user's values
 	t.ModifiedDueDay = t.UserDueDay
